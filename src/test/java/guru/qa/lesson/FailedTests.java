ba928e816ba429475749b63fe641ebd3969e755b
QAmilord <i@qamilord.ru>
2022-04-30 21:39:32 +0300

    added tests

---
+++ src/test/java/guru/qa/lesson/FailedTests.java
@@ -0,0 +1,33 @@
+package guru.qa.lesson;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FailedTests {
+
+    @Test
+    public void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void test01() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void test02() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void test03() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void test04() {
+        assertTrue(false);
+    }
+}
---
+++ src/test/java/guru/qa/lesson/SkippedTests.java
@@ -0,0 +1,27 @@
+package guru.qa.lesson;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    public void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled
+    public void test01() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled("Some reason")
+    public void test02() {
+        assertTrue(false);
+    }
+}
